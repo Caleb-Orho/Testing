cc4b3eb621e5c59930201a3c8324a1a6cde605ea
Caleb-Orho <orho0002@algonquinlive.com>
2023-08-10 14:58:27 -0400

    rr

---
+++ Bio/src/assets/index.js
@@ -0,0 +1,13 @@
+
+import Content from "./Content";
+import Header from "./Header";
+import StarsCanvas from "./Stars"
+import AniCanvas from "./Ani"
+
+
+export {
+  Content,
+  Header,
+  StarsCanvas,
+  AniCanvas
+};
